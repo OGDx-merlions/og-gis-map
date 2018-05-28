cc1c8a5673d7f921b70abe24f71eee7b2c74ee63
Ezhil Vendhan <uibuddha@gmail.com>
2018-05-28 16:43:16 +0800

    Add icon

--- px-map-behavior-icon.es6.js
+++ px-map-behavior-icon.es6.js
@@ -362,16 +362,15 @@
       <g id="MOP-${id}" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="1a.-Ministry-dashboard-Copy-3-${id}" transform="translate(-1113.000000, -429.000000)">
             <g id="Group-2-${id}" transform="translate(1114.000000, 430.000000)">
-                <circle id="Oval-4-${id}" stroke="#9B9B9B" stroke-width="2" cx="60.5" cy="39.5" r="36.5"></circle>
-                <g id="Group-Copy-7-${id}" transform="translate(0.000000, 22.000000)">
-                    <text id="Eastern-Desert-${id}" font-family="GEInspiraSans-Regular, GE Inspira Sans" font-size="15" font-weight="normal" fill="#4A4A4A">
-                        <tspan x="75" y="65">${regionLabel}</tspan>
-                    </text>
-                    <circle id="Oval-3-${id}" stroke="#9B9B9B" fill="${col}" cx="22.5" cy="41.5" r="22.5"></circle>
-                    <text id="12-${id}" font-family="GEInspiraSans-Regular, GE Inspira Sans" font-size="36" font-weight="normal" fill="#59717F">
-                        <tspan x="45.848" y="28">${oil.count}</tspan>
-                    </text>
-                </g>
+                <circle id="Oval-4-${id}" stroke="#9B9B9B" stroke-width="2" cx="64.5" cy="39.5" r="39.5"></circle>
+                <text id="Eastern-Desert-${id}" font-family="GEInspiraSans-Regular, GE Inspira Sans" font-size="15" font-weight="normal" fill="#4A4A4A">
+                    <tspan x="70" y="88">${regionLabel}</tspan>
+                </text>
+                <circle id="Oval-3-${id}" stroke="#9B9B9B" fill="${col}" cx="22.5" cy="63.5" r="22.5"></circle>
+                <path d="M13.2965521,61.7696442 L20.4648222,58.726894 C20.6596173,57.1509388 21.9800861,55.9319854 23.5800476,55.9319854 C24.2994919,55.9319854 24.9624234,56.1784544 25.491907,56.5930231 L31.6262816,53.9891356 L32.8760374,56.9333756 L31.4300079,57.5471786 L31.4300079,73.5237666 L32,73.5237666 C32.5522847,73.5237666 33,73.9714819 33,74.5237666 L33,75 L14.1600953,75 L14.1600953,74.5237666 C14.1600953,73.9714819 14.6078105,73.5237666 15.1600953,73.5237666 L20.4400635,73.5237666 L21.4355766,61.7895631 L14.5463078,64.7138842 L15.4976591,66.9551275 C15.6055566,67.2093179 15.4869627,67.5028481 15.2327723,67.6107455 L11.8177212,69.0603487 C11.5635308,69.1682461 11.2700006,69.0496522 11.1621032,68.7954618 L8.04338728,61.4482275 C7.93548985,61.1940371 8.05408374,60.9005069 8.30827414,60.7926095 L11.7233252,59.3430063 C11.9775156,59.2351089 12.2710458,59.3537028 12.3789432,59.6078932 L13.2965521,61.7696442 Z M26.6921933,59.5582616 C26.5922588,60.3199034 26.2291879,60.9967349 25.6991699,61.490774 L26.7200318,73.5237666 L29.8600159,73.5237666 L29.8600159,58.2136007 L26.6921933,59.5582616 Z M23.5800476,60.7297439 C24.4471303,60.7297439 25.1500397,60.013734 25.1500397,59.1304911 C25.1500397,58.2472481 24.4471303,57.5312382 23.5800476,57.5312382 C22.712965,57.5312382 22.0100556,58.2472481 22.0100556,59.1304911 C22.0100556,60.013734 22.712965,60.7297439 23.5800476,60.7297439 Z" id="Combined-Shape-Copy-5" fill="#FFFFFF"></path>
+                <text id="12-${id}" font-family="GEInspiraSans-Regular, GE Inspira Sans" font-size="36" font-weight="normal" fill="#59717F">
+                    <tspan x="48.848" y="50">${oil.count}</tspan>
+                </text>
             </g>
         </g>
       </g>
